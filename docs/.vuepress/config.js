d5bee4da3534513af8e062f9b4e1bf65ad7cc051
Foo-x <www.sand.x@gmail.com>
2019-02-05 23:06:29 +0900

    Add ppm

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,8 @@ module.exports = {
           [ '/strategy/5f', '1.3. 5F' ],
           [ '/strategy/7s', '1.4. 7S' ],
           [ '/strategy/pest', '1.5. PEST' ],
-          [ '/strategy/risk-map', '1.6. リスクマップ' ],
+          [ '/strategy/ppm', '1.6. PPM' ],
+          [ '/strategy/risk-map', '1.7. リスクマップ' ],
         ]
       },
       {
